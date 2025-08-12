97d8c7bbe8059270101172d31e65e9e5ab0dd025
millahh <millahmillah37@gmail.com>
2025-08-12 20:22:00 +0700

    add simple CRUD routes

---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,27 @@
+const getAllTasks =  (req, res) => {
+    res.send(req.body)
+}
+
+const getATask =  (req, res) => {
+    res.json({ id: req.params.id })
+}
+
+const createTask =  (req, res) => {
+    res.json(req.body)
+}
+
+const updateTask =  (req, res) => {
+    res.send('update task')
+}
+
+const deleteTask =  (req, res) => {
+    res.send('delete task')
+}
+
+module.exports = {
+    getAllTasks,
+    getATask,
+    createTask,
+    updateTask,
+    deleteTask
+}
--- 03-task-manager/starter/routes/tasks.js
+++ 03-task-manager/starter/routes/tasks.js
@@ -1,8 +1,20 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.route('/').get((req, res) => {
-    res.send('all items')
-})
+const {
+  getAllTasks,
+  getATask,
+  createTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers/tasks");
 
-module.exports = router
+router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getATask).patch(updateTask).delete(deleteTask);
+
+// instead of hardcoding the value, just pass the function
+// router.route('/').get((req, res) => {
+//     res.send('all items')
+// })
+
+module.exports = router;
