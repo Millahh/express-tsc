4a7719f719be55591b909385f3dce53aeb0726ca
millahh <millahmillah37@gmail.com>
2025-08-13 20:17:51 +0700

    add model and a simple Create function

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,3 +1,5 @@
+const Task = require('../models/Task')
+
 const getAllTasks =  (req, res) => {
     res.send(req.body)
 }
@@ -6,8 +8,9 @@ const getATask =  (req, res) => {
     res.json({ id: req.params.id })
 }
 
-const createTask =  (req, res) => {
-    res.json(req.body)
+const createTask = async (req, res) => {
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
 }
 
 const updateTask =  (req, res) => {
