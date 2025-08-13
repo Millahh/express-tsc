61a2f77582b7c71f1d748e282fd99c5ea79efb6d
millahh <millahmillah37@gmail.com>
2025-08-13 22:17:55 +0700

    add error handling (get and create)

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,30 +1,54 @@
-const Task = require('../models/Task')
+const Task = require("../models/Task");
 
-const getAllTasks =  (req, res) => {
-    res.send(req.body)
-}
+const getAllTasks = async (req, res) => {
+  // error handling that shows response on the back side
+  try {
+    //use .find to display all the collections
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
 
-const getATask =  (req, res) => {
-    res.json({ id: req.params.id })
-}
+const getATask = async (req, res) => {
+  // error handling that shows response on the back side
+  try {
+    //use .findOne to display collections that has matched _id
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    //for special case (_id with the same length of characters but not matching)
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
 
 const createTask = async (req, res) => {
-    const task = await Task.create(req.body)
-    res.status(201).json({ task })
-}
+  // error handling that shows response on the back side
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
 
-const updateTask =  (req, res) => {
-    res.send('update task')
-}
+const updateTask = (req, res) => {
+  res.send("update task");
+};
 
-const deleteTask =  (req, res) => {
-    res.send('delete task')
-}
+const deleteTask = (req, res) => {
+  res.send("delete task");
+};
 
 module.exports = {
-    getAllTasks,
-    getATask,
-    createTask,
-    updateTask,
-    deleteTask
-}
+  getAllTasks,
+  getATask,
+  createTask,
+  updateTask,
+  deleteTask,
+};
--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://mila:FbqIoPyXhsWIWvlQ@cluster0.l6vp9rl.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority&appName=Cluster0";
-
 const connectDB = (url) =>{
 mongoose.connect(url)
 }
