36009355cab27a6ef606007d1f9276539d28fc0e
millahh <millahmillah37@gmail.com>
2025-08-14 22:40:02 +0700

    add routes

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+require('express-async-errors')
 
 //async errors
 
@@ -6,6 +7,7 @@ const express = require('express')
 const app = express()
 
 const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
 
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
@@ -14,13 +16,12 @@ const errorMiddleware = require('./middleware/error-handler')
 app.use(express.json())
 
 // routes
-
-app.get('/', (Req, res) => {
+app.get('/', (req, res) => {
     res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
 })
 
 // products route
-
+app.use('/api/v1/products', productsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorMiddleware)
