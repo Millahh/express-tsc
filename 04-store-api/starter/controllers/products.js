db3bbd8f36a3a798bd6a83a09f3dadb8a3210970
millahh <millahmillah37@gmail.com>
2025-08-15 17:56:42 +0700

    add data-filters

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,17 +1,28 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ page: "2" }); // for specific output, type ({ any_property: value})
+  const search = 'a'
+  const products = await Product.find({
+    name: { $regex:search, $options:'i'}
+    // $regex = in this case, %value%
+    // $options: 'i' = case INsensitive
+  }); // for specific output, type ({ any_property: value}). exm: name:'millah'
   res.status(200).json({ products, nbHits: products.length });
   // u can use express-async-error package aswell
   // throw new Error('testing async errors')
 };
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company, name } = req.query;
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex:search, $options:'i'};
   }
   console.log(queryObject);
 
