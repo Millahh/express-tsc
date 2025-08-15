f74e0edf372760fc6f97678f0e333bc453851f12
millahh <millahmillah37@gmail.com>
2025-08-15 11:36:29 +0700

    add populate of the products

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,7 +1,10 @@
+const Product = require('../models/product')
+
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('testing async errors')
-    // using express-async-error package instead
-    // res.status(200).json({ msg: 'products testing route' })
+    const products = await Product.find({}) // for specific output, type ({ any_property: true})
+    res.status(200).json({ products })
+    // u can use express-async-error package aswell
+    // throw new Error('testing async errors')
 }
 const getAllProducts = async (Request, res) => {
     res.status(200).json({ msg: 'products route' })
