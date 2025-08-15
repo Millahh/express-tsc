eecd7aa3eaf68f898051bc76471ecbd6309f1f30
millahh <millahmillah37@gmail.com>
2025-08-15 11:06:54 +0700

    add model

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,5 +1,7 @@
 const getAllProductsStatic = async (req, res) => {
-    res.status(200).json({ msg: 'products testing route' })
+    throw new Error('testing async errors')
+    // using express-async-error package instead
+    // res.status(200).json({ msg: 'products testing route' })
 }
 const getAllProducts = async (Request, res) => {
     res.status(200).json({ msg: 'products route' })
