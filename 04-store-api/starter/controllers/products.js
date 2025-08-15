afaeb9a46eb3217438873c546626845d4140aab9
millahh <millahmillah37@gmail.com>
2025-08-15 17:09:40 +0700

    add filter

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,16 +1,25 @@
-const Product = require('../models/product')
+const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({}) // for specific output, type ({ any_property: true})
-    res.status(200).json({ products })
-    // u can use express-async-error package aswell
-    // throw new Error('testing async errors')
-}
-const getAllProducts = async (Request, res) => {
-    res.status(200).json({ msg: 'products route' })
-}
+  const products = await Product.find({ page: "2" }); // for specific output, type ({ any_property: value})
+  res.status(200).json({ products, nbHits: products.length });
+  // u can use express-async-error package aswell
+  // throw new Error('testing async errors')
+};
+const getAllProducts = async (req, res) => {
+  const { featured } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  console.log(queryObject);
+
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
+};
 
 module.exports = {
-    getAllProducts,
-    getAllProductsStatic,
-}
+  getAllProducts,
+  getAllProductsStatic,
+};
