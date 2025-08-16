0a796b003806349bf78ae194238e9ea06b500124
millahh <millahmillah37@gmail.com>
2025-08-16 22:14:19 +0700

    use http-status-codes

--- 05-JWT-Basics/starter/errors/bad-request.js
+++ 05-JWT-Basics/starter/errors/bad-request.js
@@ -1,11 +1,14 @@
 // extended from CustomAPIError
-const CustomAPIError = require("./custom-error")
+const CustomAPIError = require("./custom-error");
+const { StatusCode } = require("http-status-codes");
 
 class BadRequest extends CustomAPIError {
   constructor(message) {
-    super(message)
-    this.statusCode = 400
+    super(message);
+    // this.statusCode = 400
+    // instead of typing it manual, better use the ttp-status-codes package built-in
+    this.statusCode = StatusCode.BAD_REQUEST;
   }
 }
 
-module.exports = BadRequest
+module.exports = BadRequest;
--- 05-JWT-Basics/starter/errors/unauthenticate.js
+++ 05-JWT-Basics/starter/errors/unauthenticate.js
@@ -1,11 +1,14 @@
 // extended from CustomAPIError
-const CustomAPIError = require("./custom-error")
+const CustomAPIError = require("./custom-error");
+const { StatusCode } = require("http-status-codes");
 
 class UnauthenticatedError extends CustomAPIError {
   constructor(message) {
-    super(message)
-    this.statusCode = 401
+    super(message);
+    // this.statusCode = 4 00
+    // instead of typing it manual, better use the ttp-status-codes package built-in
+    this.statusCode = StatusCode.UNAUTHORIZED;
   }
 }
 
-module.exports = UnauthenticatedError
+module.exports = UnauthenticatedError;
