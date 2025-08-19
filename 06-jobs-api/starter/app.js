d67b90e4137587e3fc6876e3a12877abb9db1172
millahh <millahmillah37@gmail.com>
2025-08-19 21:37:03 +0700

    add extra securities

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xxs-clean')
+const rateLimiter = require('express-rate-limit')
 
 // connectDB
 const connectDB = require('./db/connect')
@@ -14,9 +19,19 @@ const jobsRouter = require('./routes/jobs')
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+const helmet = require('helmet');
 
+// extra security
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+}))
 app.use(express.json());
-// extra packages
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+ 
 
 // routes
 app.use('/api/v1/auth', authRouter)
