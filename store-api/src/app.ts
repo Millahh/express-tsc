462858a24ef7ce6017cd5da8c31b3c95907fd8d0
millahh <millahmillah37@gmail.com>
2025-09-05 19:01:51 +0700

    add extra security and edit some configuration

--- store-api/src/app.ts
+++ store-api/src/app.ts
@@ -6,10 +6,36 @@ import connectDB from "./db/connect";
 import notFoundMiddleware from "./middleware/not-found";
 import errorMiddleware from "./middleware/error-handler";
 import path from 'path'
+// extra security packages
+import helmet from 'helmet'
+import cors from 'cors'
+import xssClean from "xss-clean";
+import rateLimit from 'express-rate-limit'
 
-// load env var
-dotenv.config()
-const app: Application = express()
+//Load env var
+dotenv.config();
+const app:Application = express()
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "blob:"],
+      workerSrc: ["'self'", "blob:"],
+    },
+  })
+);
+
+// extra security
+app.set('trust proxy', 1)
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+}))
+app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xssClean());
 
 // middleware
 app.use(express.static(path.join(__dirname, "../public")));
